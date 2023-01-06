f25661a9a9fb16269632b31d2b745a26f9111e9d
Reimar Rosas <reimar@reimarrosas.me>
2023-01-06 10:47:02 -0500

    Frameworks install and initial setup

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.9.4/css/bulma.min.css"
+        integrity="sha512-HqxHUkJM0SYcbvxUw5P60SzdOTy/QVwA1JJrvaXJv4q7lmbDZCmZaqz01UPOaQveoxfYRv1tHozWGPMcuTBuvQ=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <script defer src="https://unpkg.com/alpinejs@2.x.x/dist/cdn.min.js"></script>
+    <script src="https://kit.fontawesome.com/659ec9636a.js" crossorigin="anonymous"></script>
+    @vite('resources/js/app.js')
+    <title>B-Log | {{ $title ?? 'Home' }}</title>
+</head>
+
+<body>
+    {{ $slot }}
+</body>
+{{ $scripts ?? '' }}
+
+</html>
